55d9127f59ef9ce586e856c28e5f1cc1a8c21a0c
rwblickhan <rwblickhan@gmail.com>
2017-08-22 19:23:34 -0700

    Initial commit

---
+++ greenthumb/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  greenthumb
+//
+//  Created by Russell Blickhan on 8/22/17.
+//  Copyright © 2017 rwblickhan. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let mainViewController = UIViewController()
+        let navController = UINavigationController(rootViewController: mainViewController)
+        window!.rootViewController = navController
+        window!.makeKeyAndVisible()
+        return true
+    }
+
+}
+
